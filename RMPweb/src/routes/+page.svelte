630f0b67abcfb013553a39f0db8ba02e492074ef
pynappo <lehtien.david@gmail.com>
2024-04-23 03:02:07 -0700

    chore: add prettier and format

--- RMPweb/src/routes/+page.svelte
+++ RMPweb/src/routes/+page.svelte
@@ -1,28 +1,27 @@
 <script lang="ts">
-    import SearchBar from "./searchbar.svelte";
-    let school:string = '';
-    let course:string = '';
-    
+  import SearchBar from "./searchbar.svelte";
+  let schools = [{}];
+  let school: string = "";
+  let course: string = "";
 </script>
 
-
-<div class='search-box'>
-    <div class="card w-96 bg-base-100 shadow-xl bg-primary">
-        <div class="card-body">
-        <div class="search-box-container">
-            <h1 class="text-3xl font-bold">RateMyProfessors.help</h1>
-            Example:<br>
-            School - San Jose State University<br>
-            Course - CS 46A
-            <SearchBar input="School" bind:output={school}/>
-            <SearchBar input="Course" bind:output={course}/>
-            <div class="card-actions justify-end">
-                <button class="btn btn-secondary">Search</button>
-            </div>
+<div class="search-box">
+  <div class="card w-96 bg-base-100 shadow-xl bg-primary">
+    <div class="card-body">
+      <div class="search-box-container">
+        <h1 class="text-3xl font-bold">RateMyProfessors.help</h1>
+        Example:<br />
+        School - San Jose State University<br />
+        Course - CS 46A
+        <SearchBar input="School" bind:output={school} />
+        <SearchBar input="Course" bind:output={course} />
+        <div class="card-actions justify-end">
+          <button class="btn btn-secondary">Search</button>
         </div>
-        </div>
+      </div>
     </div>
   </div>
+</div>
 
 <!-- <style lang="postcss">
     :global(html) {
