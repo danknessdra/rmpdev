3baac38fca902fe2500de28009f6734461c2a935
pynappo <lehtien.david@gmail.com>
2024-05-05 03:26:46 -0700

    update web UI

--- RMPweb/src/routes/+page.svelte
+++ RMPweb/src/routes/+page.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
   import type { PageData } from "./$types.js";
   import * as Form from "$lib/components/ui/form";
-  import { Input } from "$lib/components/ui/input";
+  import * as Tabs from "$lib/components/ui/tabs";
   import { formSchema, type FormSchema } from "./search/schema";
-  import Searchbar from "./searchbar.svelte";
+  import Searchbar from "./Searchbar.svelte";
   import * as Card from "$lib/components/ui/card";
   import { superForm } from "sveltekit-superforms";
   import { zodClient } from "sveltekit-superforms/adapters";
   import { type Selected } from "bits-ui";
   import type { SearchRequest } from "@elastic/elasticsearch/lib/api/types.js";
+  // import { browser, building, dev, version } from "$app/environment";
 
   export let data: PageData;
   const form = superForm(data.form, {
-    validators: zodClient(formSchema),
-    // onUpdated: ({ form: f }) => {
-    //   console.log(f);
+    validators: zodClient(formSchema), // onUpdated: ({ form: f }) => { console.log(f);
     // },
   });
 
@@ -22,72 +21,97 @@
 
   let schools: Selected<string>[] = [
     { value: "U2Nob29sLTg4MQ==", label: "SJSU" },
+    { value: "asdf", label: "SJSU" },
   ];
   let courses: Selected<string>[] = [];
   let disableCourse = true;
   let disableSubmit = true;
-  let query: SearchRequest;
+  let elasticSearchRequest: SearchRequest;
+  // let trySaveSearch = () => {};
+  // let savedSearches: FormSchema[] = [];
+  // if (browser) {
+  //   savedSearches = localStorage.getItem("savedSearches") ?? [];
+  //   trySaveSearch = (e: SubmitEvent) => {
+  //     console.log(e);
+  //     const formData = new FormData(e.target as HTMLFormElement);
+  //     savedSearches = [
+  //       ...savedSearches,
+  //       {
+  //         schoolId: formData.get("schoolId"),
+  //         course: formData.get("course"),
+  //       }, ]; };
+  // }
 </script>
 
 <div
   class="w-full h-full flex flex-col justify-center items-center align-middle"
 >
-  <Card.Root>
-    <Card.Header>
-      <Card.Title>RateMyProfessors.help</Card.Title>
-      <Card.Description>Search for a school...</Card.Description>
-    </Card.Header>
-    <Card.Content>
-      <form method="POST" use:enhance>
-        <Form.Field {form} name="schoolId">
-          <Form.Control let:attrs>
-            <Searchbar
-              items={schools}
-              bind:value={$formData.schoolId}
-              placeholder="School name"
-              inputProps={attrs}
-              onSelectedChange={() => {
-                disableCourse = true;
-                query = {
-                  index: "books",
-                  query: {
-                    match_all: {},
-                  },
-                };
-                fetch("/api/search/", {
-                  method: "POST",
-                  body: JSON.stringify(query),
-                })
-                  .then((res) => res.json())
-                  .then((result) => {
-                    courses = result;
-                    console.log(courses);
-                    disableCourse = false;
-                  });
-              }}
-            />
-          </Form.Control>
-          <Form.FieldErrors />
-        </Form.Field>
-        <Form.Field {form} name="course">
-          <Form.Control let:attrs>
-            <Searchbar
-              items={schools}
-              bind:value={$formData.course}
-              placeholder="Course name"
-              inputProps={attrs}
-              disabled={disableCourse}
-              onSelectedChange={() => {
-                disableSubmit = false;
-              }}
-            />
-          </Form.Control>
-          <Form.FieldErrors />
-        </Form.Field>
-        <Form.Button class="btn btn-secondary" disabled={disableSubmit}>
-          Search
-        </Form.Button>
-      </form>
-    </Card.Content>
-  </Card.Root>
+  <Tabs.Root value="search" class="w-[400px]">
+    <Tabs.List class="w-full grid grid-cols-2">
+      <Tabs.Trigger value="search">Search</Tabs.Trigger>
+      <Tabs.Trigger value="savedSearches">Saved Searches</Tabs.Trigger>
+    </Tabs.List>
+    <Tabs.Content value="search">
+      <Card.Root class="min-w-40">
+        <Card.Header>
+          <Card.Title>RateMyProfessors.help</Card.Title>
+          <Card.Description>Search for a school...</Card.Description>
+        </Card.Header>
+        <form method="POST" use:enhance>
+          <Card.Content>
+            <Form.Field {form} name="schoolId">
+              <Form.Control let:attrs>
+                <Searchbar
+                  items={schools}
+                  bind:value={$formData.schoolId}
+                  placeholder="School name"
+                  inputProps={attrs}
+                  on:selectedchange={(event) => {
+                    console.log(event);
+                    disableCourse = true;
+                    elasticSearchRequest = {
+                      index: "books",
+                      query: {},
+                    };
+                    fetch("/api/search/", {
+                      method: "POST",
+                      body: JSON.stringify(elasticSearchRequest),
+                    })
+                      .then((res) => res.json())
+                      .then((result) => {
+                        courses = result;
+                        disableCourse = false;
+                      });
+                  }}
+                />
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+            <Form.Field {form} name="course">
+              <Form.Control let:attrs>
+                <Searchbar
+                  items={courses}
+                  bind:value={$formData.course}
+                  placeholder="Course name"
+                  inputProps={attrs}
+                  disabled={disableCourse}
+                  on:selectedchange={() => {
+                    disableSubmit = false;
+                  }}
+                />
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+          </Card.Content>
+          <Card.Footer class="w-full flex justify-end">
+            <!-- <Button>Reset</Button> -->
+            <Form.Button disabled={disableSubmit}>Search</Form.Button>
+          </Card.Footer>
+        </form>
+      </Card.Root>
+    </Tabs.Content>
+    <Tabs.Content value="savedSearches">
+      <!-- to be done -->
+    </Tabs.Content>
+  </Tabs.Root>
 </div>
---
+++ RMPweb/src/routes/Searchbar.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { Combobox, type Selected } from "bits-ui";
+  import { Separator } from "$lib/components/ui/separator";
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  export let items: Selected<string>[] = [];
+  export let value = "";
+  export let touchedInput = false;
+  export let placeholder = "";
+  export let inputProps = {};
+  export let disabled = false;
+  $: filteredItems =
+    value && touchedInput
+      ? items.filter((item) =>
+          item.label?.toLowerCase().includes(value.toLowerCase()),
+        )
+      : items;
+</script>
+
+<Combobox.Root
+  bind:inputValue={value}
+  bind:touchedInput
+  onSelectedChange={(event) => {
+    dispatch("selectedchange", event);
+  }}
+  bind:disabled
+>
+  <!-- classes are taken from the shadcn input -->
+  <Combobox.Input
+    {placeholder}
+    class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm
+    ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium
+    placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring
+    focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+  />
+  <Combobox.Label />
+  <Combobox.Content class="bg-white border-solid border-2 rounded-md p-2">
+    {#each filteredItems as item, i (item.value)}
+      {#if i > 0}
+        <Separator />
+      {/if}
+      <Combobox.Item {...item} class="hover:bg-blue-200 p-2">
+        <Combobox.ItemIndicator />
+        {item.label}
+      </Combobox.Item>
+    {/each}
+    <Combobox.Separator />
+  </Combobox.Content>
+  <Combobox.Arrow />
+  <Combobox.HiddenInput {...inputProps} />
+</Combobox.Root>
